efd40ca3e80bb0968e691f0200af959d119bf653
OscarNR7 <oscar.alonso23.34@gmail.com>
2024-10-27 19:02:25 -0600

    Cambio deploy2

--- sistema/settings.py
+++ sistema/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t#tkm%(qvte#l^_v=#s)+*&u$%enigk2zg*yesjprk70%g2g(3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
