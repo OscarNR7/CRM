4948ab4dca795d41c810443247a026712afc5d9b
OscarNR7 <oscar.alonso23.34@gmail.com>
2024-10-27 20:23:15 -0600

    Agregada vista temporal para crear superusuario

--- sistema/settings.py
+++ sistema/settings.py
@@ -207,3 +207,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '')
 MEDIA_URL = '/clientes/fotos/'
+
+ADMIN_SECRET_TOKEN = os.environ.get('ADMIN_SECRET_TOKEN', 'un_token_secreto_temporal')
