90e0480448f173762b7022c5f5986dd314575f0c
OscarNR7 <oscar.alonso23.34@gmail.com>
2024-11-02 19:29:23 -0600

    Configuracion de rama, actu requi.txt y sqlite como prueba

--- sistema/settings.py
+++ sistema/settings.py
@@ -95,11 +95,10 @@ WSGI_APPLICATION = 'sistema.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
